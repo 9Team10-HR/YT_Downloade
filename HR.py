05e56d16414f3bbf912e263f54122b905f5d896a
MD HR <137395946+9Team10-HR@users.noreply.github.com>
2024-08-05 14:40:32 +0600

    Create HR.py

---
+++ HR.py
@@ -0,0 +1,38 @@
+import sys 
+from pytube import YouTube 
+username = "habib"
+password = '123'
+
+logo = """ \033[0;31m ╭╮╱╱╭┳━━━━╮╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╭╮
+┃╰╮╭╯┃╭╮╭╮┃╰╮╭╮┃╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱┃┃
+╰╮╰╯╭┻╯┃┃╰╯╱┃┃┃┣━━┳╮╭╮╭┳━╮┃┃╭━━┳━━┳━╯┣━━┳━╮
+╱╰╮╭╯╱╱┃┃╱╱╭╯╰╯┃╭╮┃╰╯╰╯┃╭╮┫┃┃╭╮┃╭╮┃╭╮┃┃━┫╭╯
+╱╱┃┃╱╱╱┃┃╱╱╰━━━┻━━╯╰╯╰╯╰╯╰┻━┻━━┻╯╰┻━━┻━━┻╯
+-------------------------------------------"""
+
+ 
+givenusername = input("\033[0;36mEnter your Username : ")
+ 
+if givenusername == username : 
+  print("\033[0;35mCorrect username ")
+else : 
+   print("\033[0;31mWorong Pass")
+givenpassword = input("\033[0;33mEnter Your Pass: ")
+
+if givenpassword == password : 
+  print("correct password ")
+else : 
+   print("wrong password")
+   sys.exit ()
+   
+print(logo)
+
+link = input("Enter the YouTube video URL: ")
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
